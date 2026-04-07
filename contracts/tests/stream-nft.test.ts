2c6a43c263b99b8bc09101c94a4ddc081dcf8ec0
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-04-07 19:39:25 +0100

    test(contracts): add TODO scenario placeholders for stream-nft tests

--- contracts/tests/stream-nft.test.ts
+++ contracts/tests/stream-nft.test.ts
@@ -2,4 +2,7 @@ import { describe } from "@hirosystems/clarinet-sdk";
 
 describe("stream-nft", () => {
   // TODO: add setup for stream-nft contract tests
+  // TODO: add SIP-009 mint and metadata checks
+  // TODO: add transfer authorization and ownership checks
+  // TODO: add receipt lifecycle edge-case assertions
 });
