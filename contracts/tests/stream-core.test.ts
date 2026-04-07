dcc956350869279d3e344b8012ca333d3bd62e08
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-04-07 19:26:26 +0100

    test(contracts): add TODO scenario placeholders for stream-core tests

--- contracts/tests/stream-core.test.ts
+++ contracts/tests/stream-core.test.ts
@@ -2,4 +2,7 @@ import { describe } from "@hirosystems/clarinet-sdk";
 
 describe("stream-core", () => {
 	// TODO: add setup for stream-core contract tests
+	// TODO: add stream creation and cancellation scenarios
+	// TODO: add rate accrual and fee-path assertions
+	// TODO: add authorization and edge-case coverage
 });
