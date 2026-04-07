093404000073aa299572955c0dac37f12843884d
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-04-07 05:57:19 +0100

    docs(packages): add packages/sdk README and source placeholders

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,21 +1,4 @@
-// TODO: Main SDK entry point
-// Export all public APIs from this file
-//
-// Planned exports:
-// - StreamPayClient: Main client for interacting with contracts
-// - createStream: Create a new payment stream
-// - claimPayment: Claim accrued payments from a stream
-// - cancelStream: Cancel an active stream
-// - getStream: Fetch stream details by ID
-// - listStreams: List streams for a given address
+// TODO: Define public exports for @streampay/sdk.
+// TODO: Add client APIs and helper modules.
 
 export {};
-
-// TODO: Export types
-// export type { Stream, StreamConfig, PaymentClaim } from './types';
-
-// TODO: Export client
-// export { StreamPayClient } from './client';
-
-// TODO: Export utilities
-// export { calculateClaimableAmount, formatStreamAmount } from './utils';
