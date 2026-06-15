bbe26a222c84120c753f2b2902c9f05558e50f71
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-06-15 09:00:56 +0100

    chore(web): export NotificationBell and NotificationPanel from components index

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -2,4 +2,7 @@ export { Navbar } from "./Navbar";
 export { WalletConnect } from "./WalletConnect";
 export { AddressDisplay } from "./AddressDisplay";
 export { AddressInput } from "./AddressInput";
+export { NotificationBell } from "./NotificationBell";
+export { NotificationPanel } from "./NotificationPanel";
+
 
