feb66dd7927620cd18efe22412c688a2b953edf6
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-06-15 08:53:37 +0100

    chore(web): export AddressDisplay and AddressInput from components index

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,2 +1,5 @@
 export { Navbar } from "./Navbar";
 export { WalletConnect } from "./WalletConnect";
+export { AddressDisplay } from "./AddressDisplay";
+export { AddressInput } from "./AddressInput";
+
